38e89003f6b38d3f0475bde7a92170acd2a1ceb6
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-04-20 18:37:53 -0500

    done with the landmarks side

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,38 +1,38 @@
 class LandmarksController < ApplicationController
   # add controller methods
 
-  # index 
   get '/landmarks' do
     @landmarks = Landmark.all
     @figures = Figure.all
     erb :'landmarks/index'
   end
 
-  # new 
-  get "/landmarks/new" do
+  get '/landmarks/new' do
     erb :"landmarks/new"
   end
 
-  get "/landmarks/:id" do
-    @landmarks = Landmark.find(params[:id])
-    erb :"/landmarks/show"
-  end
-
-  get "/landmarks/:id/edit" do 
-    @landmark = Landmark.find(params[:id])  
-    erb :"/landmarks/edit"
-  end
-
-  post "/landmarks/:id" do
+  get '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
-    @landmark.name = params["landmark"]["name"]
-    @landmark.year_completed = params["landmark"]["year_completed"]
+    erb :"landmarks/show"
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
+
+  # edits landmark
+  post '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
+    @landmark.save
     redirect to "/landmarks/#{@landmark.id}"
   end
 
-  post "/landmarks" do
-    Landmark.create(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"])
-    redirect to "/landmarks"
+  post '/landmarks' do
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+    redirect '/landmarks'
   end
 
 end
