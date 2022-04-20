4293c242d192bee967a50c35a5eb90d3112ca37f
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-04-20 09:37:51 -0500

    created figures folder for erbs files

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,22 @@
 class FiguresController < ApplicationController
   # add controller methods
+
+  # index
+  get "/figures" do
+    @figures = Figure.all
+    erb :"figures/index"
+  end
+
+  # new 
+  get "/figures/new" do 
+    erb :"figures/new"
+  end
+
+
+
+
+
+
+
+
 end
